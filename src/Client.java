eb5c14612f4af00f3fd37cb438670e2cae454e70
andres <andres@andres-NC-E5-551G-F371>
2018-10-23 18:25:22 +0200

    Added the ssl handshake

--- src/Client.java
+++ src/Client.java
@@ -1,50 +1,298 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.Scanner;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
 
 public class Client {
-	
-	String keyStoreName;
-	KeyStore keyStore;
-	String trustStoreName;
-	KeyStore trustStore;
-	char[] passphrase;
-	InetAddress host;
-	int port;
-	
+
+	static String keyStoreName;
+	static KeyStore keyStore;
+	static String trustStoreName;
+	static KeyStore trustStore;
+	static char[] passphrase;
+	static InetAddress host;
+	static int port;
+	static String suite;
+
 	public static void main(String[] args) {
 
-		getArgs(args);
-		
+		if( getArgs(args) < 0)
+			return;
+
+		try {
+
+			host = InetAddress.getLocalHost();
+
+		} catch (UnknownHostException e1) {
+
+			e1.printStackTrace();
+			return;
+		}
+
+		port = 5555;
+
+		try {
+
+			suite = getSuite();
+
+		} catch (NoSuchAlgorithmException e) {
+
+			e.printStackTrace();
+			return;
+		}
+
 		while(true) {
-			
+
 			int action = getAction();
-			
+
 			switch (action) {
 			case 1:
 
-				registerDoc();
+				Object o = registerDoc();
+				if(o == null) {
+
+					return;
+				}
 				break;
 
 			case 2:
-				
-				listDocs();
+
+				Object o1 = listDocs();
+				if(o1 == null) {
+
+					return;
+				}
 				break;
+				
 			case 3:
-				
-				recoverDoc();
+
+				Object o2 = recoverDoc();
+				if(o2 == null) {
+
+					return;
+				}
 				break;
-				
+
 			case 4:
-				
+
 				System.out.println("Exiting");
 				System.out.println("Goodbye");
 				return;
-				
+
 			default:
 
-				System.out.println("Something went odd");
-				System.out.println("Goodbye");
+				System.out.println("Wrong action");
+				break;
 			}
+		}
+	}
+
+	private static String getSuite() throws NoSuchAlgorithmException {
+
+		String selectedSuite;
+
+		SSLContext ctx = SSLContext.getInstance("TLS");
+		SSLSocketFactory factory = ctx.getSocketFactory();
+		String[] suites = factory.getSupportedCipherSuites();
+
+		System.out.println("Supported suites:");
+		for(int i = 0; i < suites.length; i++) {
+
+			String suite = suites[i];
+			System.out.print(i + ") " + suite);
+		}
+
+		Scanner in = new Scanner(System.in);
+
+		selectedSuite = suites[in.nextInt()];
+
+		in.close();
+
+		return selectedSuite;
+	}
+
+	private static Object recoverDoc() {
+
+		setPassphrase();
+		setKeyStoreAndTruStore();
+		Socket socket = setConnection();
+
+		if(socket == null)
+			return null;
+		
+		try {
+			
+			socket.close();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			return null;
+		}
+		
+		return null;
+	}
+
+	private static Object listDocs() {
+
+		setPassphrase();
+		setKeyStoreAndTruStore();
+		Socket socket = setConnection();
+
+		if(socket == null)
+			return null;
+		
+		try {
+			
+			socket.close();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			return null;
+		}
+		
+		return null;
+	}
+
+	private static Object registerDoc() {
+
+		setPassphrase();
+		setKeyStoreAndTruStore();
+		Socket socket = setConnection();
+
+		if(socket == null)
+			return null;
+
+		try {
+			
+			socket.close();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			return null;
+		}
+		
+		return null;
+	}
+
+	private static Socket setConnection() {
+
+		SSLSocket socket;
+
+		try {
+
+			SSLSocketFactory factory = null;
+			try {
+
+				SSLContext ctx;
+				KeyManagerFactory kmf;
+				TrustManagerFactory tmf;
+
+				ctx = SSLContext.getInstance("TLS");
+				kmf = KeyManagerFactory.getInstance("SunX509");	 	
+				tmf = TrustManagerFactory.getInstance("SunX509");
+
+				kmf.init(keyStore, passphrase);
+				tmf.init(trustStore);
+				ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+
+				factory = ctx.getSocketFactory();
+
+			} catch (Exception e) {
+
+				throw new IOException(e.getMessage());
+			}
+
+			socket = (SSLSocket)factory.createSocket(host, port);
+
+			String[] suiteArray = {suite};
+			socket.setEnabledCipherSuites(suiteArray);
+
+			socket.startHandshake();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+			return null;
+		}
+
+		return socket;
+	}
+
+	private static void setKeyStoreAndTruStore() {
+
+		try {
+
+			keyStore = KeyStore.getInstance("JKCE");
+			keyStore.load(new FileInputStream(keyStoreName), passphrase);
+
+			trustStore = KeyStore.getInstance("JKCE");
+			trustStore.load(new FileInputStream(trustStoreName), passphrase);
+
+		} catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private static void setPassphrase() {
+
+		System.out.println("Insert KeyStore password:");
+
+		Scanner in = new Scanner(System.in);
+
+		passphrase = in.nextLine().toCharArray();
+
+		in.close();
+	}
+
+	private static int getAction() {
+
+		int action = 4;
+
+		System.out.println("Select action:");
+		System.out.println("1) Register document");
+		System.out.println("2) List documents");
+		System.out.println("3) Recover document");
+		System.out.println("4) Exit");
+
+		Scanner in = new Scanner(System.in);
+
+		action = in.nextInt();
+
+		in.close();
+
+		return action;
+	}
+
+	private static int getArgs(String[] args) {
+
+		if(args.length != 2) {
+
+			System.out.println("Wrong parameters!");
+			System.out.println("Client keyStoreFile trustStoreFile");
+
+			return -1;
+		} else {
+
+			keyStoreName = args[0];
+			trustStoreName = args[1];
+
+			return 1;
 		}
 	}
 
--- src/Server.java
+++ src/Server.java
@@ -1,43 +1,143 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
 
 public class Server {
 
-	KeyStore keyStore;
-	TrustStore trustStore;
-	char[] passphrase;
-	int port;
-	
+	static KeyStore keyStore;
+	static KeyStore trustStore;
+	static char[] passphrase;
+	static int port;
+	static String cipheringAlgorithm;
+
 	public static void main(String[] args) {
 
-		getArgs(args);
+		if( getArgs(args) < 0)
+			return;
+
+		port = 5555;
 		
 		while(true) {
-			
-			waitForConection();
-			
+
+			Socket socket = waitForConection();
+
 			int request  = getRequest();
-			
+
 			switch (request) {
 			case 1:
-				
+
 				registerDocResponse();
 				break;
-				
+
 			case 2:
-				
+
 				listDocsResponse();
 				break;
-				
+
 			case 3:
-				
+
 				recoverDocResponse();
 				break;
 
 			default:
-				
+
 				System.out.println("Something went odd");
 				System.out.println("Goodbye");
 				return;
 			}
+		}
+	}
+
+	private static Socket waitForConection() {
+
+		Socket socket;
+
+        try {
+        	
+			SSLServerSocketFactory ssf = null;
+			try {
+				
+			    SSLContext ctx;
+			    KeyManagerFactory kmf;
+			    TrustManagerFactory tmf;
+
+			    ctx = SSLContext.getInstance("TLS");
+			    kmf = KeyManagerFactory.getInstance("SunX509");
+			    tmf = TrustManagerFactory.getInstance("SunX509");
+			    
+			    kmf.init(keyStore, passphrase);
+			    tmf.init(trustStore);
+			    ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+
+			    ssf = ctx.getServerSocketFactory();
+			    
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}    
+
+			ServerSocket ss = ssf.createServerSocket(port);
+			
+			((SSLServerSocket)ss).setNeedClientAuth(true);
+			
+			socket = ss.accept();
+		
+        } catch (IOException e) {
+
+        	e.printStackTrace();
+        	return null;
+		}
+	
+		return socket;
+	}
+
+	private static int getArgs(String[] args) {
+
+		if(args.length != 4) {
+
+
+			System.out.println("Wrong parameters");
+			System.out.println("Server keyStoreFile KeyStorePassword trustStoreFile cipheringAlgorithm");
+			return -1;
+
+		} else {
+
+			try {
+				
+				String keyStoreName = args[0];
+				String keyStorePassword = args[1];
+
+				passphrase = keyStorePassword.toCharArray();
+
+				keyStore = KeyStore.getInstance("JKCE");
+				keyStore.load(new FileInputStream(keyStoreName), passphrase);
+
+				String trustStoreName = args[2];
+
+				trustStore = KeyStore.getInstance("JKCE");
+				trustStore.load(new FileInputStream(trustStoreName),passphrase);
+
+				cipheringAlgorithm = args[3];
+
+			} catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException e) {
+				// TODO Auto-generated catch block
+
+				e.printStackTrace();
+				return -1;
+			}
+
+			return 1;
 		}
 	}
 
